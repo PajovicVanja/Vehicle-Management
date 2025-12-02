e2a7869a8babcfe0e0370244baed81dee92128de
PajovicVanja <vanja.pajovic@student.um.si>
2025-12-02 17:38:33 +0100

    feat: add Browsee web SDK for user tracking and analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import browsee from '@browsee/web-sdk';
+
+browsee.init({ apiKey: '85fdc35745de9780b98d14445b14a0c15dff71646ccae766' });
 
 const container = document.getElementById('root');
 createRoot(container).render(<App />);
